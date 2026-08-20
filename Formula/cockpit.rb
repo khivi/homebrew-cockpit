3785fd1d39549723a86b7eb8b0a6e52aa3aa7b81
khivi <khivi@users.noreply.github.com>
2026-08-20 13:13:50 -0400

    cockpit 1.12.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/cockpit.rb
+++ Formula/cockpit.rb
@@ -17,8 +17,8 @@ class Cockpit < Formula
 
   desc "Git-worktree workspaces for cmux/limux, aligned to GitHub PRs"
   homepage "https://github.com/khivi/cockpit"
-  url "https://github.com/khivi/cockpit/archive/refs/tags/v1.11.0.tar.gz"
-  sha256 "1ec709f1335449c654518c85eb7e7cd3566c4ac42eb883e5691646bf13804593"
+  url "https://github.com/khivi/cockpit/archive/refs/tags/v1.12.0.tar.gz"
+  sha256 "27938e188a038dc6c733678f22eed082ffcdae9a40d796c4fca46fe4d543576c"
   license "MIT"
 
   depends_on "gh"
